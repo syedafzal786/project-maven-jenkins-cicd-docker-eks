1a19f3dc82220db8eba0890d51eca28982cc1e2c
syedafzal786 <syedafzal22334@gmail.com>
2024-08-08 18:22:09 +0530

    updates

---
+++ Main-ec2/main.tf
@@ -0,0 +1,46 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-025fe52e1f2dc5044"      #change ami id for different region
+  instance_type          = "t2.large"
+  key_name               = "syed2"              #change key name as per your setup
+  vpc_security_group_ids = [aws_security_group.devops-project-umar.id]
+  user_data              = templatefile("./install.sh", {})
+
+  tags = {
+    Name = "project-MainEc2"
+  }
+
+  root_block_device {
+    volume_size = 40
+  }
+}
+
+resource "aws_security_group" "devops-project-umar" {
+  name        = "devops-project-umar"
+  description = "Allow TLS inbound traffic"
+
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000, 3000, 8082, 8081] : {
+      description      = "inbound rules"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-project-umar"
+  }
+}
+
